0d3e449bd89c7a0e233c97795a15092434a2bcf2
Nagarjuna Reddy Guntaka <guntakaarjuna@gmail.com>
2023-05-09 19:33:08 -0700

    Initial Commit

---
+++ TermProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TermProject
+//
+//  Created by Raja Shekhar Reddy Guntaka on 5/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TermProject/TermProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TermProjectApp.swift
+//  TermProject
+//
+//  Created by Raja Shekhar Reddy Guntaka on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct TermProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
